40f5d8632cac2cc2c46d46ab23357c7e85d8b4e6
ZeyadZanaty <zeyad_zanaty@hotmail.com>
2018-07-31 12:48:31 +0200

    Fix routing module

--- api/client/social-net/src/app/app-routing.module.ts
+++ api/client/social-net/src/app/app-routing.module.ts
@@ -1,19 +1,27 @@
 import { NgModule }  from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './component/login/login.component';
-import { HomeComponent } from './component/home/home.component';
-import { ProfileComponent } from './component/profile/profile.component';
-import { SignupComponent } from './component/signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { SignupComponent } from './components/signup/signup.component';
 
-
-@NgModule({
-  exports: [ RouterModule ]
-})
 const routes: Routes = [
-  { path: '/', component: LoginComponent },
-  { path: '/home', component: HomeComponent },
-  { path: '/profile/{id}', component: ProfileComponent },
-  { path: '/signup', component: SignupComponent },
+  { path: '', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'profile/{id}', component: ProfileComponent },
+  { path: 'signup', component: SignupComponent },
 
 ];
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+
+})
+
 export class AppRoutingModule {}
