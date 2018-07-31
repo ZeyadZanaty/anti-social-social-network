f31cdf22793b74c800c74afbff488d98fbbc6cc4
ZeyadZanaty <zeyad_zanaty@hotmail.com>
2018-07-31 12:28:26 +0200

    Add routes to routing module

--- api/client/social-net/src/app/app-routing.module.ts
+++ api/client/social-net/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule }  from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './component/login/login.component';
+import { HomeComponent } from './component/home/home.component';
+import { ProfileComponent } from './component/profile/profile.component';
+import { SignupComponent } from './component/signup/signup.component';
+
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+const routes: Routes = [
+  { path: '/', component: LoginComponent },
+  { path: '/home', component: HomeComponent },
+  { path: '/profile/{id}', component: ProfileComponent },
+  { path: '/signup', component: SignupComponent },
+
+];
+export class AppRoutingModule {}
