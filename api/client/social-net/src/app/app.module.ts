70ec8d8bfd2bd571b06010bd23dec0322300c670
ZeyadZanaty <zeyad_zanaty@hotmail.com>
2018-07-31 12:22:11 +0200

    Add angular file hierarchy

---
+++ api/client/social-net/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { UserComponent } from './components/user/user.component';
+import { PostComponent } from './components/post/post.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { HomeComponent } from './components/home/home.component';
+import { EditUserComponent } from './components/edit-user/edit-user.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    UserComponent,
+    PostComponent,
+    ProfileComponent,
+    HomeComponent,
+    EditUserComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
